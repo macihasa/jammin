ba705c7b30806c7f453b707d952183b508765fac
macihasa <macihasa@gmail.com>
2022-02-18 02:53:31 +0100

    Sixth commit: Added a savePlaylist function for the save button and edited the Readme

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import Playlist from './components/Playlist/Playlist';
 import { useState } from 'react';
 
 function App() {
-  // States ---------
+  // States: searchResult(array of objects), playlistName(string), playlistTracks(array of objects) trackURIs(array)
+
   const [searchResults, setsearchResults] = useState([
     {
       id: '1',
+      uri: '11',
       name: 'Best friend',
       artist: 'Billy Joel',
       album: 'Long lasting journey',
     },
     {
       id: '2',
+      uri: '22',
       name: 'Purple Haze',
       artist: 'Jimmy Hendrix',
       album: 'Purple Haze',
@@ -26,21 +29,24 @@ function App() {
   const [playlistTracks, setPlaylistTracks] = useState([
     {
       id: '3',
+      uri: '33',
       name: 'All of the lights',
       artist: 'Kanye West',
       album: 'Dark beautiful twisted fantasy',
     },
     {
       id: '4',
+      uri: '44',
       name: 'The Dress',
       artist: 'Dijon',
       album: 'Absolutely',
     },
   ]);
+  const [trackURIs, setTrackURIs] = useState([]);
 
-  // 1:01:39 / 2:11:42
-  // Methods/Functions-----------------------------------
+  // METHODS: addTrack removeTrack updateName savePlaylist
 
+  // Adds a track to state playlistTracks from state searchResults - onClick, (+) button
   const addTrack = (track) => {
     let tracks = playlistTracks;
 
@@ -54,6 +60,7 @@ function App() {
     }
   };
 
+  // Removes a track from state playlistTracks through filter - onClick, (-) button
   const removeTrack = (track) => {
     let tracks = playlistTracks;
     tracks = tracks.filter((currentTrack) => {
@@ -63,8 +70,19 @@ function App() {
     });
     setPlaylistTracks([...tracks]);
   };
+
+  // Updates state playlistName with the current name - onChange, input field
   const updateName = (e) => {
     setplaylistName(e.target.value);
+  };
+
+  // Updates state trackURIs with the playlist tracks uri component - onClick, savebutton.
+  const savePlaylist = () => {
+    let uri = [];
+    for (let index = 0; index < playlistTracks.length; index++) {
+      uri.push(playlistTracks[index].uri);
+    }
+    setTrackURIs([...uri]);
   };
 
   return (
@@ -79,6 +97,7 @@ function App() {
             playlistTracks={playlistTracks}
             onRemove={removeTrack}
             updateName={updateName}
+            onSave={savePlaylist}
           />
         </div>
       </div>
--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,12 +1,21 @@
 import './Playlist.css';
 import React from 'react';
 import Tracklist from '../Tracklist/Tracklist';
-const Playlist = ({ playlistName, playlistTracks, onRemove, updateName }) => {
+
+const Playlist = ({
+  playlistName,
+  playlistTracks,
+  onRemove,
+  updateName,
+  onSave,
+}) => {
   return (
     <div className="Playlist">
       <input type="text" defaultValue={playlistName} onChange={updateName} />
       <Tracklist tracks={playlistTracks} isRemoval={true} onRemove={onRemove} />
-      <button className="Playlist-save">SAVE TO SPOTIFY</button>
+      <button className="Playlist-save" onClick={onSave}>
+        SAVE TO SPOTIFY
+      </button>
     </div>
   );
 };
